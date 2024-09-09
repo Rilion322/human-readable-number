b2f08eead33b5b4ba1f518012bf946a13d7ce08b
Maksim Dubovik <max200289453@gmail.com>
2024-09-09 17:16:45 +0300

    Add my function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function toReadable (number) {
-  
+    if (number < 0 || number > 999 ) return 'error';
+    const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen','nineteen'];
+    const tens = [ '', '','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+    let result = '';
+    if (number === 0) {
+        return ones[0];
+    }
+
+    const hundred = Math.floor(number / 100);
+    const ten = Math.floor((number % 100) / 10);
+    const one = number % 10;
+
+    if (hundred > 0) {
+        result += ones[hundred] + ' hundred';
+    }
+    
+    if (ten > 1) {
+        if (result) result += ' ';
+        result += tens[ten];
+        if (one > 0) {
+            result += ' ' + ones[one];
+        }
+    } else if (ten === 1) {
+        if (result) result += ' ';
+        result += teens[one];
+    } else if (one > 0) {
+        if (result) result += ' ';
+        result += ones[one];
+    }
+
+    return result.trim();
+
 }
